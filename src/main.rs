1bd56edf07106c0da9f9134493b243366d9f8fa2
kindywu <kindywu@outlook.com>
2024-06-06 23:37:53 +0800

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use std::ops::Range;
+
+use anyhow::Result;
+use clap::{command, Parser};
+use colored::*;
+use glob::glob;
+use regex::Regex;
+use tokio::fs::File;
+use tokio::io::{AsyncBufReadExt, BufReader};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    pattern: String,
+
+    /// Number of times to greet
+    #[arg(short, long)]
+    glob: String,
+}
+
+// cargo run -- --pattern he\\w+ --glob "assets/test*.txt"
+#[tokio::main]
+async fn main() -> Result<()> {
+    // 解析命令行 rgrep ，读取正则表达式和文件
+    let args = Args::parse();
+
+    let regex = Regex::new(&args.pattern)?;
+
+    for entry in glob(&args.glob)? {
+        if let Ok(path) = entry {
+            println!("{}", path.display().to_string().blue());
+            let file = File::open(path).await?;
+
+            let mut lines = BufReader::new(file).lines();
+
+            let mut i = 0;
+            while let Some(line) = lines.next_line().await? {
+                i += 1;
+
+                if let Some(m) = regex.find(&line) {
+                    let Range { start, end } = m.range();
+                    let prefix = &line[..start];
+                    println!(
+                        "{0: >6}: {1}{2}{3}",
+                        i.to_string().blue(),
+                        prefix,
+                        &line[start..end].red(),
+                        &line[end..]
+                    );
+                }
+            }
+        }
+    }
+
+    // 按行读取文件
+    // 正则表达式匹配关键字
+    // 返回匹配的行
+    Ok(())
 }
